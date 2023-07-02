9ce9b386e623bbb309cc0c1ed49c6f7f619aa553
MontoyitaDev <simon1018226487@gmail.com>
2023-07-02 12:19:55 -0500

    summary

---
+++ HTML - TIKTOK/tutos.js
@@ -0,0 +1,7 @@
+function example() {
+    var num1 = parseInt(document.getElementById('valueone').value)
+    var num2 = parseInt(document.getElementById('valuetwo').value)
+    var summary;
+    summary = num1 + num2;
+    alert('Your result is: ' + summary)
+}
